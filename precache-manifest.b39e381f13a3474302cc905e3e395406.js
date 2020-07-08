9f3d43add10e1e9edfae600e7f0bcb8fc8d4de45
atomcorp <thomasmaxwellsmith@gmail.com>
2020-07-08 23:16:13 +0100

    Updates

---
+++ precache-manifest.b39e381f13a3474302cc905e3e395406.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4d4c426dba7a32157786c06c4f96ac0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6513e9a459479f3d98a3",
+    "url": "/static/css/main.1484ab72.chunk.css"
+  },
+  {
+    "revision": "b979ab7d9f680ce11544",
+    "url": "/static/js/2.3288fd65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3288fd65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6513e9a459479f3d98a3",
+    "url": "/static/js/main.753e4b41.chunk.js"
+  },
+  {
+    "revision": "56cac8dbd84d981a7b35",
+    "url": "/static/js/runtime-main.8f1e3687.js"
+  }
+]);
